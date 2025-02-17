016e6c0d868d44744bb3948fe6a99003c516d101
gaallmin <86390492+gaallmin@users.noreply.github.com>
2025-02-17 16:22:56 +0000

    Update models.py

--- models.py
+++ models.py
@@ -5,13 +5,13 @@ from sqlalchemy.orm import sessionmaker
 Base = declarative_base()
 
 class UserFeedback(Base):
-    __tablename__ = 'user_feedback'
+    __tablename__ = 'ankh_db'
     id = Column(Integer, primary_key=True, autoincrement=True)
     reservation_opinion = Column(Text)
     health_issues = Column(Text)
     ankh_help = Column(Text)
 
-DATABASE_URL = "mysql+pymysql://ankh:ankh03013@localhost:3306/ankh_app_db"
+DATABASE_URL = "mysql+pymysql://admin:ankh-survey-db.c78y4ugsgy3q.us-east-1.rds.amazonaws.com:3306/ankh"
 
 engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(bind=engine)
